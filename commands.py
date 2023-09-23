ae686112a6bca337d354b79eff05b42b0a39b9c9
jahaha03 <44372238+jahaha03@users.noreply.github.com>
2023-09-23 04:13:38 -0700

    small update

--- commands.py
+++ commands.py
@@ -1,12 +1,15 @@
 import datetime
-import pywhatkit as pwk
 import webbrowser
 from input_output import talk
-from spotify_access import get_token, search_song, authenticate
+from spotify_access import search_song
 
 
 urls = {
-    "youtube": "https://youtube.com"
+    "youtube": "https://youtube.com",
+    "canvas": "https://canvas.ucdavis.edu",
+    "github": "https://github.com",
+    "linkedin": "https://www.linkedin.com/in/jeremy1112ha/",
+    "gmail": "https://mail.google.com/mail/u/0/#inbox"
 }
 
 month_convert = {
@@ -17,23 +20,48 @@ month_convert = {
 
 
 def commands(instruction, sp, token):
+    
     if "play" in instruction:
         song = instruction.replace("play ", "")
         song_uri = search_song(token, song)
         sp.start_playback(uris=[song_uri])
+        
     elif "pause" in instruction:
         sp.pause_playback() 
+        
     elif "resume" in instruction:
         sp.start_playback()
+        
     elif "time" in instruction:
         time = datetime.datetime.now().strftime("%I:%M%p")
         talk(f"the time is {time}")
+        
     elif "date" in instruction:
         month = month_convert[int(datetime.datetime.now().strftime("%m"))]
         day_year = datetime.datetime.now().strftime(f"%d %Y")
         talk(f"the date is {month} {day_year}")
+        
     elif "open" in instruction:
         if "loyal listener" in instruction:
             webbrowser.open("https://jorm.pythonanywhere.com")
+            talk("opening loyal listener")
+            
         elif "youtube" in instruction:
             webbrowser.open(urls["youtube"])
+            talk("opening youtube")
+            
+        elif "canvas" in instruction:
+            webbrowser.open(urls["canvas"])
+            talk("opening canvas")
+            
+        elif "github" in instruction:
+            webbrowser.open(urls["github"])
+            talk("opening git hub")
+            
+        elif "linkedin" in instruction:
+            webbrowser.open(urls["linkedin"])
+            talk("opening linkedin")
+            
+        elif "gmail" in instruction:
+            webbrowser.open(urls["gmail"])
+            talk("opening gmail")
--- spotify_access.py
+++ spotify_access.py
@@ -14,6 +14,7 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 
 def get_token():
+    
     auth_string = f"{CLIENT_ID}:{CLIENT_SECRET}"
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
@@ -24,6 +25,7 @@ def get_token():
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {"grant_type": "client_credentials"}
+    
     result = post(url, headers=headers, data=data)
     json_result = json.loads(result.content)
     token = json_result["access_token"]
@@ -31,12 +33,15 @@ def get_token():
 
 
 def get_auth_header(token):
+    
     return {"Authorization": f"Bearer {token}"}
 
 
 def search_song(token, song_query):
+    
     url = f"https://api.spotify.com/v1/search?q={song_query}&type=track"
     headers = get_auth_header(token)
+    
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]["items"]
     # song_uri = sorted(json_result, key=lambda x: x["popularity"], reverse=True)[0]["uri"]
